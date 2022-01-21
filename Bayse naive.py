4f035c5ce5782f1d519178c47e3a9e6ba1b0a51b
OrenN <niazovoren@gmail.com>
2022-01-21 08:08:11 +0200

    Starting the decision tree

---
+++ Bayse naive.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv(r'C:\Users\orenn\OneDrive - Kornit Digital\Desktop\database\diabetes.csv')
---
+++ Decision_Tree.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class DecisionTree:
+
+    def __init__(self, X_Train_data, y_train_label):
+        self.x_train = X_Train_data
+        self.y_train = y_train_label
+
+    def gini_calc(self):
+        pass
